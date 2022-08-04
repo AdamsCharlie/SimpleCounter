2082de033ddd56e3283e1cbe95cfb66f09a83536
Charles Adams <Charles.Adams@murphyusa.com>
2022-08-04 15:45:18 -0500

    Initial Commit

---
+++ SimpleCounter/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SimpleCounter
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleCounter/Views/CounterPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SimpleCounter.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CounterPage : ContentPage
+    {
+        public CounterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            Incrementer.Value = 0;
+        }  
+    }
+}
